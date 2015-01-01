1e94d9a594cb0492b65f8ea17125b19cad885aff
William Roush <william.roush@roushtech.net>
2015-01-01 02:10:29 -0500

    Humor me with a gem file.

--- _scripts/ci/run_deploy_prepare.sh
+++ _scripts/ci/run_deploy_prepare.sh
@@ -3,6 +3,7 @@ mkdir travis-output-repo
 mv * ./travis-output-repo
 mv ./travis-output-repo/_site/* ./
 rm -R ./travis-output-repo
+echo gem 'rack' >> Gemfile
 git config user.name "travis"
 git config user.email "travis@null"
 git add -A
