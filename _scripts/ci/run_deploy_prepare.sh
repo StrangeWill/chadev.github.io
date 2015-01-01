0905af1ca52580155a9eb1fdced4a9e3be566ec2
William Roush <william.roush@roushtech.net>
2015-01-01 01:55:08 -0500

    Add in Travis git user config.

--- _scripts/ci/run_deploy_prepare.sh
+++ _scripts/ci/run_deploy_prepare.sh
@@ -3,4 +3,6 @@ mkdir travis-output-repo
 mv * ./travis-output-repo
 mv ./travis-output-repo/_site/* ./
 rm -R ./travis-output-repo
+git config user.name "travis"
+git config user.email "travis@null"
 git commit -am "Travis commit"
